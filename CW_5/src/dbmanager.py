723b0ab2716af8b66fffd6b5713131bd84c1315a
Klasis <d23kv321@gmail.com>
2023-10-08 15:33:59 +0300

    commit

--- CW_5/src/dbmanager.py
+++ CW_5/src/dbmanager.py
@@ -10,6 +10,45 @@ class DBManager:
             password=password
         )
 
+    def save_to_database(self, vacancies):
+        cur = self.conn.cursor()
+
+        for vacancy in vacancies['items']:
+            vacancy_id = vacancy['id']
+            vacancy_name = vacancy['name']
+            vacancy_salary_min = vacancy['salary']['from'] if vacancy['salary'] and 'from' in vacancy[
+                'salary'] else None
+            vacancy_salary_max = vacancy['salary']['to'] if vacancy['salary'] and 'to' in vacancy['salary'] else None
+            vacancy_url = vacancy['url']
+            vacancy_company_id = vacancy['employer']['id'] if vacancy['employer'] and 'id' in vacancy[
+                'employer'] else None
+
+            # Проверка, существует ли запись с таким id в таблице "vacancies"
+            cur.execute("""
+                SELECT id FROM vacancies WHERE id = %s
+            """, (vacancy_id,))
+            existing_vacancy = cur.fetchone()
+
+            if existing_vacancy:
+                print(f"Запись с id={vacancy_id} уже существует в таблице 'vacancies'. Пропускаем вставку.")
+                continue
+
+            # Вставка данных в таблицу "companies"
+            cur.execute("""
+                INSERT INTO companies (id, name)
+                VALUES (%s, %s)
+                ON CONFLICT (id) DO NOTHING
+            """, (vacancy_company_id, vacancy['employer']['name']))
+
+            # Вставка данных в таблицу "vacancies"
+            cur.execute("""
+                INSERT INTO vacancies (id, name, salary_min, salary_max, url, company_id)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """, (vacancy_id, vacancy_name, vacancy_salary_min, vacancy_salary_max, vacancy_url, vacancy_company_id))
+
+        self.conn.commit()
+        cur.close()
+
     def get_companies_and_vacancies_count(self):
         cur = self.conn.cursor()
         cur.execute("""
