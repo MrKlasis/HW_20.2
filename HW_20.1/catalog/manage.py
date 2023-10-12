2b6042a88e482ff3118682ddaa525108aa3a357d
Klasis <d23kv321@gmail.com>
2023-10-12 21:14:35 +0300

    commit

--- HW_20.1/catalog/manage.py
+++ HW_20.1/catalog/manage.py
@@ -2,12 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import sys
 import os
-from django.shortcuts import render
-from django.urls import path
-from django.conf import settings
-from django.core.management import execute_from_command_line
-
-
 
 
 def main():
@@ -25,28 +19,5 @@ def main():
 
 SE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def home(request):
-    return render(request, 'home.html')
-
-def contacts(request):
-    return render(request, 'contacts.html')
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('contacts', contacts, name='contacts'),
-]
-
 if __name__ == '__main__':
-    settings.configure(
-        DEBUG=True,
-        ROOT_URLCONF=__name__,
-        TEMPLATES=[
-            {
-                'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'DIRS': [os.path.join(SE_DIR, 'templates')],
-                'APP_DIRS': True,
-            },
-        ],
-    )
-    execute_from_command_line(["manage.py", "runserver"])
-
+    main()
