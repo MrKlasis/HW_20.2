f481fb70912910c6b2a8b731f91016d96f074b31
Klasis <d23kv321@gmail.com>
2023-10-12 17:40:15 +0300

    commit

--- Django_projects/catalog/manage.py
+++ Django_projects/catalog/manage.py
@@ -1,7 +1,11 @@
 #!/usr/bin/venv python
 """Django's command-line utility for administrative tasks."""
-import os
 import sys
+from django.shortcuts import render
+from django.urls import path
+from django.conf import settings
+from django.core.management import execute_from_command_line
+import os
 
 
 def main():
@@ -17,6 +21,30 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+SE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def home(request):
+    return render(request, 'home.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('contacts', contacts, name='contacts'),
+]
 
 if __name__ == '__main__':
-    main()
+    settings.configure(
+        DEBUG=True,
+        ROOT_URLCONF=__name__,
+        TEMPLATES=[
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [os.path.join(SE_DIR, 'templates')],
+                'APP_DIRS': True,
+            },
+        ],
+    )
+    execute_from_command_line(["manage.py", "runserver"])
+
